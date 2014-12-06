6cfb52ed90acc0a59fc420c9e9e2de662cdaa7cd
Fool-LeoTao <leotao19930820@gmail.com>
2014-12-06 08:25:14 +0000

    generate first revision file

---
+++ migrations/versions/2e0f9f4b42a7_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 2e0f9f4b42a7
+Revises: None
+Create Date: 2014-12-06 08:24:40.948942
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2e0f9f4b42a7'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
