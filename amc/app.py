ae92b79b791e09571b395b7cccdb182cdc0d99ff
Fool-LeoTao <leotao19930820@gmail.com>
2015-01-13 19:31:06 +0800

    first view test

--- amc/app.py
+++ amc/app.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from werkzeug.utils import import_string
 
 from amc.extensions import db, migrate
 from amc._settings import DevConfig
+
+bps = [
+    'amc.views.home:bp',
+]
 
 
 def create_app(config=DevConfig):
     app = Flask(__name__)
     app.config.from_object(config)
     register_extensions(app)
+    register_blueprints(app)
     return app
 
 
 def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
+
+
+def register_blueprints(app):
+    for bp in bps:
+        app.register_blueprint(import_string(bp))
---
+++ amc/views/home.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from flask import (request, views, Blueprint,
+                   url_for, render_template)
+
+bp = Blueprint('home', __name__)
+
+
+class HomeView(views.MethodView):
+
+    def get(self):
+        return render_template('home.html')
+
+bp.add_url_rule('/', view_func=HomeView.as_view('home'))
