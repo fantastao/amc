b31bab2c01db6cfa26bdefc945514727402c0b1c
Fool-LeoTao <leotao19930820@gmail.com>
2015-01-21 23:51:24 +0800

    use MethodView and page base structrue ensure

--- amc/views/home.py
+++ amc/views/home.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from flask import (request, views, Blueprint,
-                   url_for, render_template)
+from flask import views, Blueprint, render_template
+
 
 bp = Blueprint('home', __name__)
 
 
 class HomeView(views.MethodView):
 
+    template = 'front/home.html'
+
     def get(self):
-        return render_template('home.html')
+        return render_template(self.template)
 
 bp.add_url_rule('/', view_func=HomeView.as_view('index'))
