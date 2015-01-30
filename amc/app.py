b9775f5beeaea5ecf87eda1f403bbcd67d44c469
Fool-LeoTao <leotao19930820@gmail.com>
2015-01-30 13:49:34 +0800

    fix

--- amc/app.py
+++ amc/app.py
@@ -11,8 +11,8 @@ bps = [
     'amc.views.auth:bp',
     'amc.views.products:bp',
     'amc.views.orders:bp',
-    'amc.panel.index:bp'
-    'amc.panel.useradmin:bp',
+    'amc.panel.index:bp',
+    'amc.panel.user_admin:bp',
 ]
 
 
