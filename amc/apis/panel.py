e72c81ce92658390f6bfcc6b71a4da7579ad26ac
ljh <lijiahongasdf@163.com>
2015-03-08 21:12:09 +0800

    ljh

--- amc/apis/panel.py
+++ amc/apis/panel.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
+
 from flask import views, Blueprint, request, jsonify, abort
 
 from flask.ext.login import current_user, login_required
 
-from amc.models import OrderModel, OrderHistoryModel
+from amc.models import OrderModel, OrderHistoryModel, PayModel
 from amc.utils import now
 from amc.permissions import panel_permission
 
@@ -71,7 +73,60 @@ class OrderPanelAPI(views.MethodView):
 
         return jsonify(order.as_dict()), 200
 
+class IndexBarChartAPI(views.MethodView):
+
+    @login_required
+    @panel_permission.require(401)
+
+    def get(self):
+        results = dict()
+        timeline = now() - timedelta(days=30)
+        orders = (OrderModel.query
+                        .filter(OrderModel.date_created > timeline)
+                        .order_by(OrderModel.date_created.asc())
+                        .all())
+        for order in orders:
+            timestamp = order.date_created.strftime('%Y-%m-%d')
+            try:
+                results[timestamp][0] += 1
+                results[timestamp][1] += order.order_price
+            except KeyError:
+                results[timestamp] = [0,0]
+
+        return jsonify(results)
+
+class IndexPieChartAPI(views.MethodView):
+
+    @login_required
+    @panel_permission.require(401)
+
+    def get(self):
+        product_info = dict()
+        timeline = now() - timedelta(days=7)
+        orders = (OrderModel.query
+                        .filter(OrderModel.date_created > timeline)
+                        .all())
+        for order in orders:
+            products = order.products
+            for product in products:
+                product_id = product.product_id
+                product_name = product.product.name
+                product_quantity = product.product_quantity
+                total_price = product.total_price
+                try:
+                    product_info[product_id][1] += product_quantity
+                    product_info[product_id][2] += total_price
+                except KeyError:
+                    product_info[product_id] = [product_name, product_quantity, total_price]
+
+        return jsonify(product_info)
 
 bp.add_url_rule(
     '/order/<int:id>/',
     view_func=OrderPanelAPI.as_view('order'))
+bp.add_url_rule(
+    '/index_bar_chart/',
+    view_func=IndexBarChartAPI.as_view('index_bar_chart'))
+bp.add_url_rule(
+    '/index_pie_chart/',
+    view_func=IndexPieChartAPI.as_view('index_pie_chart'))
